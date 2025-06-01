f407ed8b1a85fd93c92d3fafc1477c1690a5dfee
Burak Altun <52549565+mburakaltun@users.noreply.github.com>
2025-06-01 16:57:50 +0300

    Initial commit

---
+++ src/main/java/com/mburakaltun/guessbuddy/GuessBuddyApplication.java
@@ -0,0 +1,13 @@
+package com.mburakaltun.guessbuddy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GuessBuddyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GuessBuddyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mburakaltun/guessbuddy/GuessBuddyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mburakaltun.guessbuddy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GuessBuddyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
